52dfa11815919b6fb8657ea63a1c19e2d005c438
Rocío Martos <rocionintendo29@gmail.com>
2024-06-16 19:08:47 +0200

    Empiezo las correcciones

---
+++ iOSAvanzado/iOSAvanzado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSAvanzado
+//
+//  Created by Rocio Martos on 16/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
